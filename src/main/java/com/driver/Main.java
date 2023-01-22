ce9d9cd74fb7b0356b070f3b7bf6885ae8051ba5
Himanshu Yadav <himanshuyadav7852@gmail.com>
2023-01-22 20:26:09 +0530

    final commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
+import java.sql.SQLOutput;
+
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+        obj.setName("Bharat");
+        System.out.println(obj.getName());
+    }
 }
